4591edbcc8657dbda642b2b5a93a23abc647e762
7love6yu <111636718+7love6yu@users.noreply.github.com>
2023-11-18 15:32:06 +0800

    Delete Python/Second directory

--- Python/Second/R7-12.py
+++
@@ -1,4 +0,0 @@
-A, B = map(int, input().split())
-numbers = [str(i) for i in range(A, B + 1)]
-result = ' '.join(numbers)
-print(result)
--- Python/Second/R7-4.py
+++
@@ -1,8 +0,0 @@
-a = input()
-a = int(a)
-
-if a % 3 == 0 and a % 5 == 0 and a % 7 == 0  :
-    print(f"Yes",end='')
-else:
-    print(f"No",end='')
-    
